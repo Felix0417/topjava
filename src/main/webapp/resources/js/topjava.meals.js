b8223442fbe7b3860e38a653ce42eeac3b630fec
felix <Felix0417>
2023-08-06 20:15:01 +0500

    Solved HW9 task with optional part

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -17,14 +17,40 @@ function clearFilter() {
     $.get(mealAjaxUrl, updateTableByData);
 }
 
+$.ajaxSetup({
+    converters: {
+        "text json": function (stringData) {
+            let json = JSON.parse(stringData);
+            if (typeof json === 'object') {
+                $(json).each(function () {
+                    if (this.hasOwnProperty('dateTime')) {
+                        this.dateTime = this.dateTime.slice(0, 16).replace('T', ' ');
+                    }
+                });
+            }
+            return json;
+        }
+    }
+});
+
 $(function () {
     makeEditable(
         $("#datatable").DataTable({
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
+                    "data": "dateTime",
+                    "render": function (date, type, row) {
+                        if (type === "display") {
+                            return date.substring(0, 16).replace('T', " ");
+                        }
+                        return date;
+                    }
                 },
                 {
                     "data": "description"
@@ -33,12 +59,14 @@ $(function () {
                     "data": "calories"
                 },
                 {
-                    "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderEditBtn
                 },
                 {
-                    "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -46,7 +74,59 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                $(row).attr("data-meal-excess", data.excess);
+            }
         })
     );
+});
+
+let startDate = $('#startDate');
+let endDate = $('#endDate');
+
+startDate.datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    formatDate: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            maxDate: endDate.val() ? endDate.val() : false
+        })
+    }
+});
+endDate.datetimepicker({
+    timepicker: false,
+    format: 'Y-m-d',
+    formatDate: 'Y-m-d',
+    onShow: function (ct) {
+        this.setOptions({
+            minDate: startDate.val() ? startDate.val() : false
+        })
+    }
+});
+
+let startTime = $('#startTime');
+let endTime = $('#endTime');
+startTime.datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            maxTime: endTime.val() ? endTime.val() : false
+        })
+    }
+});
+endTime.datetimepicker({
+    datepicker: false,
+    format: 'H:i',
+    onShow: function (ct) {
+        this.setOptions({
+            minTime: startTime.val() ? startTime.val() : false
+        })
+    }
+});
+
+$('#dateTime').datetimepicker({
+    format: "Y-m-d H:i"
 });
